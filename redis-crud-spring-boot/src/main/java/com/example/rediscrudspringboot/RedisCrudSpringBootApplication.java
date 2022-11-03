1799fcb80f9c641a1575aad8cd705fd3c8154f36
phuong <phuongdenpro@gmail.com>
2022-11-03 23:27:02 +0700

    first commit

---
+++ redis-crud-spring-boot/src/main/java/com/example/rediscrudspringboot/RedisCrudSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.rediscrudspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCrudSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisCrudSpringBootApplication.class, args);
+	}
+
+}
