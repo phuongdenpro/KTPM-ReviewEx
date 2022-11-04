89659a45f31a3cf379b9957f0cd2f1d44b52ac5d
phuong <phuongdenpro@gmail.com>
2022-11-04 17:59:52 +0700

    api gateway

--- maybay-service/src/main/java/com/example/maybayservice/service/MayBayService.java
+++
@@ -1,9 +0,0 @@
-package com.example.maybayservice.service;
-
-import com.example.maybayservice.entity.MayBay;
-
-import java.util.List;
-
-public interface MayBayService {
-    List<MayBay> getMayBayHon10000km();
-}
